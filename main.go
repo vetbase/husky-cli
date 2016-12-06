b201ea5beed689208f53b71e0726efb231ca28c2
Lark Mullins <larkmullins@gmail.com>
2016-12-05 23:00:23 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+
+    "github.com/urfave/cli"
+)
+
+func main() {
+    // create CLI application
+    app := cli.NewApp()
+
+    // set fields
+    app.Name        = "Husky"
+    app.Usage       = "Microframework for developing services"
+    app.Version     = "0.1.0"
+    app.Commands    = registerCommands()
+
+    // return CLI app
+    app.Run(os.Args)
+}
+
+func registerCommands() []cli.Command {
+    return []cli.Command{
+        {
+            Name:   "run",
+            Usage:  "Run Husky service",
+            Action: runService,
+        },
+    }
+}
+
+func runService(c *cli.Context) error {
+    _, err := exec.Command("go", "run", "./main.go").Output()
+
+    if err == nil {
+        fmt.Println("==> Running Husky service")
+    }
+
+    return nil
+}
